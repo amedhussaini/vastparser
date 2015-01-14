072366cec131558aeb9430f240e785dc7a75baef
amedhussaini <amedhussaini@gmail.com>
2015-01-14 15:26:12 -0500

    Separating Creative child elements.

--- js/vast.js
+++ js/vast.js
@@ -11,20 +11,66 @@ $('#vast-url-button').click(function(){
 //		})
 
 // count creatives, traverse each, display table based on creative #
+	
+		var numberOfCreatives = $(data).find('Creatives').children().length;
+		var creatives = $(data).find('Creatives').children();
+		console.log(numberOfCreatives);
+		console.log(creatives);
+		console.log($(data).find('Creatives'));
+		
+		var creativeParentNumber = 1;
 
-		var numberTrackingEvents = $(data).find('TrackingEvents').length;
+
+		$(creatives).each(function(index, element){
+			
+			
+			//var trackingEvents = $(this).find('TrackingEvents').children();
+			
+			$(this).each(function(index, element){
+				
+				//$('#tracking').append('<tr><td>' + creativeNumber + '</td><td>' + $(this).attr('event') + '</td><td>' + $(this).text()  + '</td></tr>');
+				var trackingEvents = $(this).find('TrackingEvents').children();
+				
+				$(trackingEvents).each(function(index, element){
+			
+					$('#tracking').append('<tr><td>' + creativeParentNumber + '</td><td>' + $(this).attr('event') + '</td><td>' + $(this).text()  + '</td></tr>');
+
+				});
+
+				
+
+			});
+
+			creativeParentNumber++;
+		
+		});
+
+/*
+
+		
 
 		var trackingEvents = $(data).find('TrackingEvents').children();
 
 		$(trackingEvents).each(function(index, element){
 			
-			console.log($(this).attr('event'));
 			$('#tracking').append('<tr><td>' + $(this).attr('event') + '</td><td>' + $(this).text()  + '</td></tr>');
-			console.log($(this).text());
-		
+
 		});
 
+		var mediaFiles = $(data).find('MediaFiles').children();
 
+		$(mediaFiles).each(function(index, element){
 
+			var url = $(this).text();
+			var bitrate = $(this).attr('bitrate');
+			var height = $(this).attr('height');
+			var width = $(this).attr('width');
+			var type = $(this).attr('type');
+
+			$('#mediafiles').append('<tr><td>' + url + '</td><td>' + bitrate + '</td><td>' + height + '</td><td>' + width + '</td><td>' + type + '</td></tr>');
+
+		});
+
+*/
 	});
 });
