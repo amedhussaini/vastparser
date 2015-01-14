ad0d11365499d8678185e776ded91aca840dacc5
amedhussaini <amedhussaini@gmail.com>
2015-01-14 16:56:54 -0500

    Traversing from the top now.

--- js/vast.js
+++ js/vast.js
@@ -12,38 +12,76 @@ $('#vast-url-button').click(function(){
 
 // count creatives, traverse each, display table based on creative #
 	
-		var numberOfCreatives = $(data).find('Creatives').children().length;
-		var creatives = $(data).find('Creatives').children();
-		console.log(numberOfCreatives);
-		console.log(creatives);
-		console.log($(data).find('Creatives'));
+		// TOP LEVEL VAST INFORMATION
+
+
+		var ADS = $(data).find('VAST').children();
+
+		var STR_VERSION = $(data).find('VAST').attr('version');
+		var STR_NUMBER_OF_ADS = $(data).find('VAST').children().length;
+
+		$('#meta-information').append('<li>Version: ' + STR_VERSION + '</li>');
+		$('#meta-information').append('<li># of Ads: ' + STR_NUMBER_OF_ADS + '</li>');
 		
-		var creativeParentNumber = 1;
+		$(ADS).each(function(index, element) {
+			
+			// INLINE LEVEL DATA
 
+			var STR_ADSYSTEM = $(this).find('AdSystem').text();
+			var STR_ADTITLE = $(this).find('AdTitle').text();
+			var STR_DESCRIPTION = $(this).find('Description').text();
+			var IMPRESSION_TRACKERS = $(this).find('Impression');
 
-		$(creatives).each(function(index, element){
+			var INLINE = $(this).find('InLine');
 			
-			
-			//var trackingEvents = $(this).find('TrackingEvents').children();
-			
-			$(this).each(function(index, element){
+			// CREATIVES
+
+			var CREATIVES = $(INLINE).find('Creatives').children();
+
+			var creativeParentNumber = 1;
+
+			$(CREATIVES).each(function(index, element){
 				
-				//$('#tracking').append('<tr><td>' + creativeNumber + '</td><td>' + $(this).attr('event') + '</td><td>' + $(this).text()  + '</td></tr>');
-				var trackingEvents = $(this).find('TrackingEvents').children();
-				
-				$(trackingEvents).each(function(index, element){
-			
-					$('#tracking').append('<tr><td>' + creativeParentNumber + '</td><td>' + $(this).attr('event') + '</td><td>' + $(this).text()  + '</td></tr>');
+				$(this).each(function(index, element){
+					//$('#tracking').append('<tr><td>' + creativeNumber + '</td><td>' + $(this).attr('event') + '</td><td>' + $(this).text()  + '</td></tr>');
+					
+					var STR_TYPE = null;
 
+					// SUPERR EFFICIENT IFSS!!!!!!!
+
+					if ($(this).find('Linear').length == 1) {
+						STR_TYPE = 'Linear Ad';
+					} else if ($(this).find('CompanionAds').length == 1) {
+						STR_TYPE = 'Companion Banner'
+					} else if ($(this).find('NonLinearAds').length == 1) {
+						STR_TYPE = 'NonLinear Ad';
+					} else  {
+						STR_TYPE = "UNKNOWN!";
+					}
+
+					console.log(STR_TYPE);
+
+					$('#ads').append('<h3>Creative #' + creativeParentNumber + ' (' + STR_TYPE + ')</h3><ul id="ad-' + creativeParentNumber + '"></ul>');
+					var trackingEvents = $(this).find('TrackingEvents').children();
+					$(trackingEvents).each(function(index, element){
+						
+						//$('#tracking').append('<tr><td>' + creativeParentNumber + '</td><td>' + $(this).attr('event') + '</td><td>' + $(this).text()  + '</td></tr>');
+						$('#ad-' + creativeParentNumber).append('<li><b>' + $(this).attr('event') + ':</b> ' + $(this).text() + '</li>');
+					});
 				});
-
-				
-
+				creativeParentNumber++;
 			});
 
-			creativeParentNumber++;
-		
+			
 		});
+
+
+		//var numberOfCreatives = $(data).find('Creatives').children().length;
+		//var creatives = $(data).find('Creatives').children();
+		
+		//console.log(creatives);
+
+		
 
 /*
 
