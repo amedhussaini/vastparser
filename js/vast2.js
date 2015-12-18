9835a65da862fb3de356c283257cfb1930cf1a5f
amedhussaini <amedhussaini@gmail.com>
2015-12-18 13:44:25 -0800

    0.0.0.3
    -continued efforts to report back all elements of the XML tag

--- js/vast2.js
+++ js/vast2.js
@@ -5,6 +5,8 @@ var t3 = (function($) {
     var debug = false;
     var tag = "http://ad.doubleclick.net/pfadx/N3158.129263.YUME/B8446438.114271426;sz=0x0;ord=$%7Brand%7D;dcmt=text/xml;yume_xml_timeout=10000;yume_ad_timeout=10000";
     //var tag = "https://bs.serving-sys.com/BurstingPipe/adServer.bs?cn=is&c=23&pl=VAST&pli=14272792&PluID=0&pos=7918&ord=${rand}&cim=1";
+    //var tag = "http://fw.adsafeprotected.com/vast/fwjsvid/st/47149/6673121/skeleton.js?originalVast=https://bs.serving-sys.com/BurstingPipe/adServer.bs?cn=is&c=23&pl=VAST&pli=15243107&PluID=0&pos=1000&ord=${rand}&cim=1";
+    //var tag = "http://bs.serving-sys.com/BurstingPipe/adServer.bs?cn=is&c=23&pl=VAST&pli=13959650&PluID=0&pos=9578&ord=${rand}&cim=1&yume_xml_timeout=10000&yume_ad_timeout=10000";
     function setTag(uri) {
         tag = uri;
     }
@@ -23,9 +25,8 @@ var t3 = (function($) {
             context = {};
             context.impression_trackers = [];
             context.study_trackers = [];
-            context.creatives_companionbanners = [];
-            context.creatives_vast = [];
-            context.creatives_vpaid = [];
+            context.creatives = [];
+            context.mediafiles = [];
 
             context.version = $(data).find('VAST').attr("version");
             context.number_of_ads = $(data).find('VAST').children().length
@@ -35,23 +36,20 @@ var t3 = (function($) {
             var _creatives = $(data).find('Creatives').children();
 
             $(_impression_trackers).each(function(index, element) {
-
                 context.impression_trackers.push({url: $(this).text(), provider: _getVendor($(this).text())});
-
             });
 
             $(_study_trackers).each(function(index,element) {
-
                 context.study_trackers.push({url: $(this).text(), provider: _getVendor($(this).text())});
             });
 
             $(_creatives).each(function(index, element) {
-
-                console.log($(this));
-
+                var media_files = $(this).find('MediaFiles').children();
+                $(media_files).each(function(index, element) {
+                    var media_files = [];
+                    context.mediafiles.push({type: $(this).attr("type"), url: $(this).text()});
+                });
             });
-
-
 
             var source   = $("#template").html();
             var template = Handlebars.compile(source);
@@ -94,6 +92,38 @@ var t3 = (function($) {
 
     }
 
+    function _getTypeOfCreative(object) {
+
+        var type = null;
+
+        if (object.find('CompanionAds').length > 0) {
+            type = "Companion Banner";
+        } else {
+
+            var media_files = object.find('MediaFiles').children();
+
+            $(media_files).each(function(index, element) {
+
+                switch($(this).attr("type")) {
+                    case "application/shockwave-flash":
+                        type = "VPAID (Flash)";
+                    case "application/x-shockwave-flash":
+                        type = "VPAID (Flash)";
+                    case "application/javascript":
+                        type = "VPAID (JavaScript)";
+                    case "application/x-javascript":
+                        type = "VPAID (JavaScript)";
+                    default:
+                        type = "VAST";
+                }
+
+            });
+        }
+
+        return type;
+
+    }
+
     return {
         setTag: setTag,
         getTag: getTag,
@@ -113,6 +143,5 @@ Handlebars.registerHelper('list', function(items, options) {
     for(var i=0, l=items.length; i<l; i++) {
         out += options.fn(items[i]);
     }
-    console.log(out);
     return out;
 });
