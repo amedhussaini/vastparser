30fa3cdd9f1d2a073fd15277338686ac9fc36a02
amedhussaini <amedhussaini@gmail.com>
2016-01-07 11:29:47 -0800

    empty feed detection

--- js/vast2.js
+++ js/vast2.js
@@ -1,9 +1,11 @@
 var t3 = (function($, window) {
 
-    // internal settings
+    // private variables
 
     var debug = false;
-    var tag = "http://rtr.innovid.com/r1.560ebd0d9c0ac6.53548068;cb=${rand}";
+    var tag = "http://vast.brandads.net/vast?line_item=13649676&subid1=novpaid&ba_cb=${rand}";
+    var _type = null;
+    var _creativeType = null;
     //var tag = "https://experiences.fuiszmedia.com/5633bf0b9ede1703001a3dae/vast.xml";
     //var tag = "http://fw.adsafeprotected.com/vast/fwjsvid/st/47149/6673121/skeleton.js?originalVast=https://bs.serving-sys.com/BurstingPipe/adServer.bs?cn=is&c=23&pl=VAST&pli=15243107&PluID=0&pos=1000&ord=${rand}&cim=1";
     //var tag = "http://bs.serving-sys.com/BurstingPipe/adServer.bs?cn=is&c=23&pl=VAST&pli=13959650&PluID=0&pos=9578&ord=${rand}&cim=1&yume_xml_timeout=10000&yume_ad_timeout=10000";
@@ -20,9 +22,15 @@ var t3 = (function($, window) {
     }
 
     function parseTag() {
+
+        _type = null;
+        _creativeType = null;
+
         $.get(tag).done(function(data) {
 
             context = {};
+            //can haz cheeseburger
+            context.has_ads = true;
             context.impression_trackers = [];
             context.study_trackers = [];
             context.creatives = [];
@@ -38,10 +46,22 @@ var t3 = (function($, window) {
             context.number_of_secure_trackers = 0;
             context.number_of_general_urls = 0;
 
+            var _ads = $(data).find('Ad');
+            var _length_ads = _ads.length;
+            var _length_ads_children = $(_ads).children().length;
+
+
+
             var _impression_trackers = $(data).find('Impression');
             var _study_trackers = $(data).find('Survey');
             var _creatives = $(data).find('Creatives').children();
             var _trackingEvents = $(data).find('Creatives').children();
+
+
+            // check if the feed is lacking ad elements or has an ad element with no children
+            if (_length_ads == 0 || _length_ads_children == 0) {
+                context.has_ads = false;
+            } 
 
             // impression elements
             $(_impression_trackers).each(function(index, element) {
@@ -113,6 +133,8 @@ var t3 = (function($, window) {
                             $(co_ad_elements).each(function(index, element) {
 
                                 if (element.nodeName != 'TrackingEvents') {
+                                    _incrementGeneralUrls($(this).text());
+                                    _checkUrlForSecurity($(this).text());            
                                     context.creatives[_index].companion_ads.push({type: element.nodeName, url: $(this).text()});
                                 }
 
@@ -123,7 +145,8 @@ var t3 = (function($, window) {
 
 
                     $(this).each(function(index, element) {
-
+                        _incrementGeneralUrls($(this).text());
+                        _checkUrlForSecurity($(this).text());
                         context.creatives_video_click.push({type: element.nodeName, url: $(this).text()});
                     });
 
@@ -167,7 +190,8 @@ var t3 = (function($, window) {
             {provider: 'BlueKai', keyword: 'bluekai.com'},
             {provider: 'Aggregate Knowledge (Neustar)', keyword: 'agkn.com'},
             {provider: 'Segment', keyword: 'segment.io'},
-            {provider: 'BrandAds', keyword: 'brandads.net'}
+            {provider: 'BrandAds', keyword: 'brandads.net'},
+            {provider: 'IAS', keyword: 'adsafeprotected.com'}
         ];
 
         dict.forEach(function(a) {
@@ -182,61 +206,75 @@ var t3 = (function($, window) {
 
     function _getTypeOfCreative(object) {
 
-        var type = null;
-
         if (object.find('CompanionAds').length > 0) {
-            type = "Companion Banner";
+            _creativeType = "Companion Banner";
         } else {
 
             var media_files = object.find('MediaFiles').children();
 
             $(media_files).each(function(index, element) {
-                switch($(this).attr("type")) {
-                    case "application/shockwave-flash":
-                        type = "VPAID (Flash)";
-                    case "application/x-shockwave-flash":
-                        type = "VPAID (Flash)";
-                    case "application/javascript":
-                        type = "VPAID (JavaScript)";
-                    case "application/x-javascript":
-                        type = "VPAID (JavaScript)";
+
+                var attribute = $(this).attr("type");
+
+
+                if (attribute == "application/shockwave-flash") {
+                    _creativeType = "VPAID (Flash)";
                 }
+                if (attribute == "application/x-shockwave-flash") {
+                    _creativeType = "VPAID (Flash)";
+                }
+                if (attribute == "application/javascript") {
+                    _creativeType = "VPAID (JavaScript)";
+                }
+                if (attribute == "application/x-javascript") {
+                    _creativeType = "VPAID (JavaScript)";
+                }
+
 
             });
         
         }
-        if (type == null) {
+        if (_creativeType == null) {
             return "VAST";
         } else {
-            return type;
+            return _creativeType;
         }
     }
 
     function _getTypeOfTag(object) {
-        var type = null;
+
 
             var media_files = object.find('MediaFiles').children();
 
             $(media_files).each(function(index, element) {
-                switch($(this).attr("type")) {
-                    case "application/shockwave-flash":
-                        type = "VPAID (Flash)";
-                    case "application/x-shockwave-flash":
-                        type = "VPAID (Flash)";
-                    case "application/javascript":
-                        type = "VPAID (JavaScript)";
-                    case "application/x-javascript":
-                        type = "VPAID (JavaScript)";
+
+                var attribute = $(this).attr("type");
+
+                //console.log(attribute);
+
+                if (attribute == "application/shockwave-flash") {
+                    _type = "VPAID (Flash)";
+                }
+                if (attribute == "application/x-shockwave-flash") {
+                    _type = "VPAID (Flash)";
+                }
+                if (attribute == "application/javascript") {
+                    _type = "VPAID (JavaScript)";
+                }
+                if (attribute == "application/x-javascript") {
+                    _type = "VPAID (JavaScript)";
                 }
 
             });
         
 
-        if (type == null) {
+        
+        if (_type == null) {
             return "VAST";
         } else {
-            return type;
+            return _type;
         }
+        
     }
 
     function _incrementTrackers() {
@@ -273,7 +311,7 @@ var t3 = (function($, window) {
 
 $(document).ready(function() {
 
-    t3.start();
+    //t3.start();
 
     $('#button-input').click(function(){
         t3.getTag();
